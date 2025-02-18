2624547734cde0abe5dc0d4a974b4df47c90ae58
servo pc <fernandonando98@live.com.mx>
2025-02-17 20:04:35 -0600

    feat: axios services in front

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,5 +1,7 @@
 import { createContext, useState, useContext } from "react";
 
+import { login as loginService, profile } from "../services/auth";
+
 //Crear contexto de autenticación
 const AuthContext = createContext();
 
@@ -8,16 +10,21 @@ export const AuthProvider = ({ children }) => {
   //Todo lo que irá dentro del contexto que sea accesible para todos los compoenentes suscritos al contexto
   const [user, setUser] = useState(null);
 
-  const login = (formData) => {
+  const login = async (formData) => {
     console.log("Ejecutando login", formData);
-    /**
-     * 1.- Mandar llamar a la API /auth/login
-     *
-     * 2.- Si sale bien el login, guardar el token en localStorage
-     *      y modificar el estado de user
-     *
-     * 3.- Si no sale bien el login enviar mensaje
-     */
+
+    // 1.- Mandar llamar a la API /auth/login
+    const token = await loginService(formData.email, formData.password);
+
+    // 2.- Si sale bien el login, guardar el token en localStorage
+    localStorage.setItem("token", token);
+
+    // 3.- Si no sale bien el login enviar mensaje
+
+    // 4.- Si nos regrasan el token mandar llamar /profile y obtener al usuario
+    const user = await profile(token);
+
+    setUser(user);
   };
 
   const logout = () => {
---
+++ Frontend/src/services/auth.js
@@ -0,0 +1,55 @@
+import api from "./index";
+
+const register = async (
+  birthday,
+  email,
+  firstName,
+  gender,
+  lastName,
+  password
+) => {
+  try {
+    const { data } = await api.post("/auth/register", {
+      birthday,
+      email,
+      firstName,
+      gender,
+      lastName,
+      password,
+    });
+    return data.code;
+  } catch (error) {
+    console.error(error);
+    console.error("Error al hacer register");
+  }
+};
+
+const login = async (email, password) => {
+  try {
+    const { data } = await api.post("/auth/login", {
+      email,
+      password,
+    });
+
+    return data.token;
+  } catch (error) {
+    console.error(error);
+    console.error("Error al hacer login");
+  }
+};
+
+const profile = async (token) => {
+  try {
+    const { data } = await api.get("/auth/profile", {
+      headers: {
+        Authorization: token,
+      },
+    });
+    return data.user;
+  } catch (error) {
+    console.error(error);
+    console.error("Error al validar token");
+  }
+};
+
+export { register, login, profile };
